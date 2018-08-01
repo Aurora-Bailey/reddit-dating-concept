c76217057f1b4584f09a8df89c6da7f65ed7b2eb
Brad Thompson <gbradthompson@gmail.com>
2018-07-31 22:28:37 -0600

    send reddit auth to authentication server

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,7 +88,8 @@
         fixed: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
+          { icon: 'star', title: 'Sign Up', to: '/signup' }
         ],
         miniVariant: false,
         right: true,
---
+++ pages/signup.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <a href="https://www.reddit.com/api/v1/authorize?client_id=6j6q8xJMzLZDYQ&response_type=code&state=signup&redirect_uri=http://localhost:3000/signup/&duration=temporary&scope=mysubreddits identity">Sign up</a>
+      window.location.search
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+  import axios from 'axios'
+  function parseSearchString (searchString) {
+    let parsed = {}
+    if (searchString.charAt(0) === '?') searchString = searchString.substring(1)
+    searchString.split('&').forEach(query => {
+      let [key, value] = query.split('=')
+      parsed[key] = value
+    })
+    return parsed
+  }
+  export default {
+    data () {
+      return {
+        adsf: 'asdf'
+      }
+    },
+    mounted () {
+      if (window && window.location && window.location.search) {
+        let searchObject = parseSearchString(window.location.search)
+        if (searchObject.error) alert(searchObject.error)
+        else axios.post('http://localhost:3412', searchObject).then(console.log).catch(console.log)
+      } else alert('no search string found')
+    }
+  }
+</script>
